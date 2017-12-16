6d090ddc32f777f29753465370fc66a3b317bb12
HirokiNakashima <hiroki123naka4ma@gmail.com>
2017-12-16 18:13:49 +0900

    initial commit

--- biblehubLink/ViewController.swift
+++ biblehubLink/ViewController.swift
@@ -9,19 +9,62 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    @IBOutlet var logoImage: NSImageView!
+    
+    @IBOutlet var bookSelect: NSPopUpButton! {
+        didSet {
+            // ヘッダー項目を設定
+            // self.bookSelect.addItemWithTitle(buttonTitle)
+            
+            // ボタンを押したときの選択肢はself.titlesに入っている情報を設定
+//            for title in self.titles {
+//                let menu
+//
+//                menu = NSMenuItem(title: title, action: selector, keyEquivalent: "")
+//                menu.target = self
+//
+//                self.popupButton.menu!.addItem(menu)
+//            }
+        }
+    }
+    
+    
+    
+    @IBOutlet var chapter: NSTextField!
+    
+    @IBOutlet var verse: NSTextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
 
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
+        
+            
+            
         }
     }
-
+    
+    
+    @IBAction func pushGo(_ sender: NSButton) {
+        // Goボタンを押したときの動作
+        
+        let domain = NSURL(string: "http://biblehub.com")
+        
+        var url = domain
+        
+//        BibleHub のURLをブラウザで起動
+    
+//        if UIApplication.shared.canOpenURL(url) {
+//            UIApplication.shared.open(url)
+//        }
+        
+    }
+    
 
 }
 
