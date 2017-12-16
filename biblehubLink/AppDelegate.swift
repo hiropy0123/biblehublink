684ee147b9e6f80494631676bf83ca377dda241c
HirokiNakashima <hiroki123naka4ma@gmail.com>
2017-12-16 17:17:17 +0900

    Initial Commit

---
+++ biblehubLink/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  biblehubLink
+//
+//  Created by Hiroki Nakashima on 2017/12/16.
+//  Copyright © 2017 Hiroki Nakashima. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ biblehubLink/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  biblehubLink
+//
+//  Created by Hiroki Nakashima on 2017/12/16.
+//  Copyright © 2017 Hiroki Nakashima. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
